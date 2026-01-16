2bb7528a67efbf81291750cec312947c37100f63
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-01-16 20:35:11 +0530

    Initial calculator app

---
+++ calculator/main.py
@@ -0,0 +1,21 @@
+# Simple Calculator App
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Cannot divide by zero"
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
